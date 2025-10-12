ab3b7c87332105ed37e9b11f37c0515b9b0b6961
italocorraro <italo.corraro@gmail.com>
2025-10-12 17:55:03 +0200

    Add files via upload

---
+++ Selectors/snippets.js
@@ -0,0 +1,22 @@
+// snippets.js
+const snippets = {
+  html: {
+    htmlCode1: ``
+  },
+  css: {
+    cssCodeElem: `p {font-size: 20px;}`,
+    cssCodeClass: `.classSelector {font-size: large;}`,
+    cssCodeID: `#idSelector {font-size: 2rem;}`,
+    cssCodeUniv: `* {font-size: 1vw;}`,
+    cssCodeOR: 
+`p.main_paragraph, #yo_mama, .apply_at_me {
+  font-weight: bold;
+}`,
+    cssCodeProx: `p + div`,
+    cssCodeMin: `p ~ div`,
+    cssCodeChild: `p > div`
+  },
+  js: {
+    jsCode1: ``
+  }
+};
