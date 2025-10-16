a9b6362d44a383e2247a587030749c924a10d756
italocorraro <italo.corraro@gmail.com>
2025-10-16 19:48:00 +0200

    Add files via upload

--- Sintassi html/snippets.js
+++ Sintassi html/snippets.js
@@ -3,7 +3,9 @@ const snippets = {
   html: {
     htmlCodeVoid: `<input type="text" value="" />`,
     htmlCodeCross: `<p><i>Questa cosa è sbagliata</p></i>`,
-    htmlCodeStyle: `<p style="width: 60px; height:40px;">`
+    htmlCodeStyle: `<p style="width: 60px; height:40px;">`,
+    htmlCodePop: `<p popover id="pop1">Ciaoo!</p>
+<button type="button" popovertarget="pop1">Clicca qui</button>`
   },
   css: {
     cssCodeCheckbox: ``
