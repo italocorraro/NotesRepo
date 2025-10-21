35b86a37f8a7eca580ff8e6d29941357c2b8ca82
italocorraro <italo.corraro@gmail.com>
2025-10-21 15:06:16 +0200

    Add files via upload

--- Sintassi html/snippets.js
+++ Sintassi html/snippets.js
@@ -5,7 +5,26 @@ const snippets = {
     htmlCodeCross: `<p><i>Questa cosa è sbagliata</p></i>`,
     htmlCodeStyle: `<p style="width: 60px; height:40px;">`,
     htmlCodePop: `<p popover id="pop1">Ciaoo!</p>
-<button type="button" popovertarget="pop1">Clicca qui</button>`
+<button type="button" popovertarget="pop1">
+    Clicca qui</button>`,
+    htmlCodeButton: 
+`<button class="htmlCodeButton" type="button" 
+        onclick="this.textContent = 'Hai cliccato!' ">
+    Clicca per vedere
+</button>`,
+    htmlCodeTemplate:
+`<!DOCTYPE html>
+<html lang="it">
+
+    <head>
+    <!-- head content -->
+    </head>
+
+    <body>
+    <!-- body content -->
+    </body>
+
+</html>`
   },
   css: {
     cssCodeCheckbox: ``
